8c32daf606f4ce48abb7e6846ed5a382250ed660
Anastasia Kishkun <Anastasia.Kishkun@skoltech.ru>
2020-05-26 22:59:57 +0300

    clean space

--- data/Data.py
+++ data/Data.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 
 from google_drive_downloader import GoogleDriveDownloader as gdd
 
@@ -8,7 +7,7 @@ class Downloader(object):
     def __init__(self, sequence_id='08', main_dir='dataset'):
         self.sequence_id = sequence_id
         self.main_dir = main_dir
-        self.sequence = Sequence(self.sequence_id)
+        self.sequence = Sequence(self.sequence_id, self.main_dir)
         if not os.path.exists(self.main_dir):
             os.mkdir(self.main_dir)
 
@@ -19,33 +18,33 @@ class Downloader(object):
                                             unzip=True)
         gdd.download_file_from_google_drive(file_id=self.sequence.images.id, dest_path=self.sequence.images.name,
                                             unzip=True)
-        self.clean_space()
+        self.clean_space(self.main_dir)
 
-    def clean_space(self):
-        shutil.move(os.path.join(os.curdir, self.sequence_id, 'image2'), os.path.join(os.curdir, self.main_dir, 'sequences', self.sequence_id, 'image2'))
-        shutil.move(os.path.join(os.curdir, self.sequence_id, 'image2'), os.path.join(os.curdir, self.main_dir, 'sequences', self.sequence_id, 'image2'))
+    def clean_space(self, main_dir):
         os.remove(self.sequence.calib.name)
         os.remove(self.sequence.poses.name)
-        os.remove(self.sequence.images.name)
-        os.remove(os.path.join(os.curdir, self.sequence_id, 'calib.txt'))
-        os.remove(os.path.join(os.curdir, self.sequence_id, 'times.txt'))
-        os.remove(os.path.join(os.curdir, self.sequence_id))
+        os.remove(os.path.join(os.curdir, main_dir, 'sequences', self.sequence.images.name))
 
 
 class Sequence(object):
-    def __init__(self, sequence_id=8):
+    def __init__(self, sequence_id='08', main_dir='dataset'):
         self.sequence_id = sequence_id
+        self.main_dir = main_dir
         self.calib = Kitti_link('data_odometry_calib.zip', '1jW1Yr8qBD2m63QQjN_q_EJWiQIyhtFj0')
-        self.images = Kitti_link('data_odometry_color.zip', '1s6GhV8UQHdZjWaX1pcJy_8TZ9rbT-21C')
         self.poses = Kitti_link('data_odometry_poses.zip', '1m1J7T_1hvrIWbT14m9KDSrffgqhUaEfL')
+        self.images = Kitti_link('data_odometry_color.zip', '1s6GhV8UQHdZjWaX1pcJy_8TZ9rbT-21C', ins=True,
+                                 main_dir=self.main_dir)
 
 
 class Kitti_link(object):
-    def __init__(self, name, id):
-        self.name = os.path.join(os.curdir, name)
+    def __init__(self, name, id, ins=False, main_dir='dataset'):
+        if ins:
+            self.name = os.path.join(os.curdir, main_dir, 'sequences', name)
+        else:
+            self.name = os.path.join(os.curdir, name)
         self.id = id
 
 
 if __name__ == '__main__':
-    s8 = Downloader(8)
+    s8 = Downloader('08')
     s8.download_sequence()
