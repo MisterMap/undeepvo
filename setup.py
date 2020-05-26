a60ece0c0f9a8993e627f76bb0a0bec9c68def27
Anastasia Kishkun <Anastasia.Kishkun@skoltech.ru>
2020-05-26 21:42:31 +0300

    data

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(name='undeepvo',
       author='Deep project team',
       author_email='',
       package_dir={},
-      packages=["undeepvo_utils"],
+      packages=["data"],
       install_requires=install_requires
       )
