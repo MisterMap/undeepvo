2a141f737459ec40b9b2e24527d9897d1da9c6ba
Anastasia Kishkun <Anastasia.Kishkun@skoltech.ru>
2020-05-27 22:02:22 +0300

    new_id

--- data/Data.py
+++ data/Data.py
@@ -1,8 +1,5 @@
 import os
-
-import pykitti
 from google_drive_downloader import GoogleDriveDownloader as gdd
-
 
 class Downloader(object):
     def __init__(self, sequence_id='08', main_dir='dataset'):
@@ -24,25 +21,19 @@ class Downloader(object):
         os.remove(os.path.join(os.curdir, self.main_dir, 'sequences', self.sequence_id, 'calib.txt'))
         os.rename(os.path.join(os.curdir, self.main_dir, 'sequences', self.sequence_id, 'calib1.txt'),
                   os.path.join(os.curdir, self.main_dir, 'sequences', self.sequence_id, 'calib.txt'))
-        self.clean_space()
-        self.pykiti_sequence()
+        self.clean_space(self.main_dir)
 
     def clean_space(self):
         os.remove(self.sequence.calib.name)
         os.remove(self.sequence.poses.name)
         os.remove(self.sequence.images.name)
-
-    def pykiti_sequence(self, frames=range(0, 20, 5)):
-        self.dataset = pykitti.odometry(self.sequence.main_dir, self.sequence.sequence_id, frames=frames)
-
-
 class Sequence(object):
     def __init__(self, sequence_id='08', main_dir='dataset'):
         self.sequence_id = sequence_id
         self.main_dir = main_dir
         self.calib = Kitti_link('data_odometry_calib.zip', '1jW1Yr8qBD2m63QQjN_q_EJWiQIyhtFj0')
         self.poses = Kitti_link('data_odometry_poses.zip', '1m1J7T_1hvrIWbT14m9KDSrffgqhUaEfL')
-        self.images = Kitti_link('data_odometry_color.zip', '1s6GhV8UQHdZjWaX1pcJy_8TZ9rbT-21C', ins=True,
+        self.images = Kitti_link('data_odometry_color.zip', '1ECFS37EURAeK8qGmOLDQ3dAxx6_Wkuxe', ins=True,
                                  main_dir=self.main_dir)
 
 
@@ -58,4 +49,3 @@ class Kitti_link(object):
 if __name__ == '__main__':
     s8 = Downloader('08')
     s8.download_sequence()
-    s8.pykiti_sequence(range(0, 100, 10))
