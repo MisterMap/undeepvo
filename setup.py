382517635329c722286af37df51aa8f112b99820
Mikhail Kurenkov <kurenkov1995@mail.ru>
2020-05-30 20:58:30 +0300

    DLP: fix setup

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(name='undeepvo',
       author='Deep project team',
       author_email='',
       package_dir={},
-      packages=["undeepvo_utils", "models", "data"],
+      packages=["undeepvo_utils", "models", "data", "unsupervised_depth_problem"],
       install_requires=install_requires
       )
