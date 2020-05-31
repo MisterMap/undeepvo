821fcc598d79ae3519960d0b2ea8d7da7becf82b
Valery Ilin <valery.ilin@skoltech.ru>
2020-05-31 12:19:24 +0300

    update datatransform manager

--- undeepvo/data/datatransform_manager.py
+++ undeepvo/data/datatransform_manager.py
@@ -6,14 +6,16 @@ class DataTransformManager:
     def get_train_transform():
         return albumentations.Compose([
             albumentations.Flip(p=0.2),
-            albumentations.Rotate(7, p=0.3),
-            albumentations.RandomBrightnessContrast(brightness_limit=(-0.2, 0.2), contrast_limit=(-0.2, 0.2), p=0.4),
+            albumentations.Rotate(10, p=0.3),
             albumentations.RandomSizedCrop(min_max_height=(112, 144), height=128, width=384),
-            albumentations.RandomGamma(),
+            albumentations.RandomBrightnessContrast(brightness_limit=(-0.1, 0.1), contrast_limit=(-0.1, 0.1), p=0.2),
+            albumentations.RandomGamma(gamma_limit=(90, 110), p=0.2),
+            albumentations.ISONoise(color_shift=(-0.1, 0.1), p=0.2),
             albumentations.ChannelShuffle(),
             albumentations.Normalize(),
             albumentations.Resize(height=128, width=384),
         ])
+
     @staticmethod
     def get_validation_transform():
         return albumentations.Compose([
