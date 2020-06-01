7ff12eb3f46b78e11105da3a0eff04da666f3490
Anastasia Kishkun <Anastasia.Kishkun@skoltech.ru>
2020-06-01 14:24:43 +0300

    mlflow_tags

--- undeepvo/utils/mflow_handler.py
+++ undeepvo/utils/mflow_handler.py
@@ -39,7 +39,7 @@ class MlFlowHandler(object):
     def start_callback(self, parameters):
         try:
             mlflow.set_tags(self._mlflow_tags)
-            mlflow.set_experiment(self._experiment_name)
+            #mlflow.set_experiment(self._experiment_name)
             mlflow.start_run()
             mlflow.log_params(parameters)
         except mlflow.exceptions.MlflowException as msg:
