fix normalisation

undeepvo/problems/unsupervised_depth_problem.py
@@ -58,16 +58,14 @@ class UnsupervisedDepthProblem(Problem):
 
     def _get_depth_figure(self):
         self._model.eval()
-        image = self._dataset_manager.get_raw_val_data()["left_current_image"]
-        image_to_plot = self._dataset_manager.make_transform_raw(np.asarray(image), resize=True, normalize=False)
-        image_to_net = torch.from_numpy(
-            self._dataset_manager.make_transform_raw(image_to_plot, resize=False, normalize=True)).permute(2, 0, 1)
-
+        image = self._dataset_manager.get_validation_dataset(with_normalize=True)[0]["left_current_image"]
         with torch.no_grad():
-            depth_image = self._model.depth(image_to_net[None].to(self._device))
+            depth_image = self._model.depth(image[None].to(self._device))
         depth_image = depth_image[0].cpu().permute(1, 2, 0).detach().numpy()[:, :, 0]
         figure, axes = plt.subplots(2, 1, dpi=150)
-        axes[0].imshow(np.clip(image_to_plot, 0, 1))
+        image = self._dataset_manager.get_validation_dataset(with_normalize=False)[0]["left_current_image"]
+        raw_image = image.cpu().permute(1, 2, 0).detach().numpy()
+        axes[0].imshow(np.clip(raw_image, 0, 1))
         axes[0].set_title("Left current image")
         axes[1].imshow(np.clip(depth_image, 0, 100) / 100, cmap="inferno")
         axes[1].set_title("Left current depth")