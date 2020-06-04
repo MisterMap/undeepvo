fix normalisation

undeepvo/problems/supervised_dataset_manager.py
@@ -5,7 +5,7 @@ from torch.utils.data import DataLoader, random_split
 
 class SupervisedDatasetManager(DatasetManager):
     def __init__(self, dataset: GroundTruthDataset, num_workers=4, lenghts=(80, 10, 10), final_img_size=(128, 384),
-                 transform_params={"filters": True, "normalize": False}):
+                 transform_params={"filters": True, "normalize": True}):
         dataset = MonoDepthDataset(dataset=dataset)
         train, val, test = random_split(dataset, lenghts)
         self._num_workers = num_workers
@@ -17,16 +17,16 @@ class SupervisedDatasetManager(DatasetManager):
         self._train_dataset.dataset.set_transform(self._transform.get_train_transform())
         return DataLoader(self._train_dataset, batch_size=batch_size, shuffle=True, num_workers=self._num_workers)
 
-    def get_validation_batches(self, batch_size, with_normalize=False):
+    def get_validation_batches(self, batch_size, with_normalize=True):
         self._validation_dataset.dataset.set_transform(
             self._transform.get_validation_transform(with_normalize=with_normalize))
         return DataLoader(self._validation_dataset, batch_size=batch_size, shuffle=False, num_workers=self._num_workers)
 
-    def get_test_batches(self, batch_size, with_normalize=False):
+    def get_test_batches(self, batch_size, with_normalize=True):
         self._test_dataset.dataset.set_transform(self._transform.get_test_transform(with_normalize=with_normalize))
         return DataLoader(self._test_dataset, batch_size=batch_size, shuffle=False, num_workers=self._num_workers)
 
-    def get_validation_dataset(self, with_normalize=False):
+    def get_validation_dataset(self, with_normalize=True):
         self._validation_dataset.dataset.set_transform(
             self._transform.get_validation_transform(with_normalize=with_normalize))
         return self._validation_dataset