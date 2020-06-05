DLP: fix pose metric, change to quaternion

test/test_pose_loss.py
@@ -3,7 +3,6 @@ import sys
 import unittest
 
 import kornia
-import numpy as np
 import pykitti.odometry
 import torch
 
@@ -26,14 +25,12 @@ class TestPoseLoss(unittest.TestCase):
             sequence_8.download_sequence()
         dataset = pykitti.odometry(sequence_8.main_dir, sequence_8.sequence_id, frames=range(0, 3, 1))
         dataset_manager = UnsupervisedDatasetManager(dataset, lenghts=(1, 1, 1), num_workers=WORKERS_COUNT)
-        camera0_from_camera2_transformation = dataset.calib.T_cam0_velo.dot(np.linalg.inv(dataset.calib.T_cam2_velo))
-        camera0_from_camera2_transformation = torch.from_numpy(camera0_from_camera2_transformation).cuda()[None].float()
-
-        camera0_from_camera3_transformation = dataset.calib.T_cam0_velo.dot(np.linalg.inv(dataset.calib.T_cam3_velo))
-        camera0_from_camera3_transformation = torch.from_numpy(camera0_from_camera3_transformation).cuda()[None].float()
-
+        camera0_from_camera2_transformation = dataset_manager.get_camera0_from_left_transformation()
+        print(camera0_from_camera2_transformation)
+        camera0_from_camera3_transformation = dataset_manager.get_camera0_from_right_transformation()
+        print(camera0_from_camera3_transformation)
         world_from_camera0_transformation = generate_transformation(torch.tensor([[1., 1., 1.]]),
-                                                                    torch.tensor([[0.1, 0.1, 0.1]])).cuda()
+                                                                    torch.tensor([[1, 2., 1, 2.]])).cuda()
         left_pose = generate_pose(kornia.compose_transformations(world_from_camera0_transformation,
                                                                  camera0_from_camera2_transformation))
         right_pose = generate_pose(kornia.compose_transformations(world_from_camera0_transformation,
@@ -42,4 +39,4 @@ class TestPoseLoss(unittest.TestCase):
         out = pose_loss(left_pose[0], right_pose[0], left_pose[1], right_pose[1])
         self.assertEqual(out.shape, torch.Size([]))
         self.assertGreaterEqual(out, 0)
-        self.assertLessEqual(out, 0.001)
+        self.assertLessEqual(out, 0.01)

undeepvo/data/pose_data_point.py
@@ -1,6 +1,6 @@
 import kornia
-import torch
 import numpy as np
+import torch
 
 
 class PoseDataPoint:
@@ -11,6 +11,11 @@ class PoseDataPoint:
         self._next_angle_name = "next_angle"
         self._delta_position_name = "delta_position"
         self._delta_angle_name = "delta_angle"
+        self._current_transformation_name = "current_transformation"
+        self._next_transformation_name = "next_transformation"
+
+        self._current_transformation = torch.from_numpy(current_matrix.astype('float32'))
+        self._next_transformation = torch.from_numpy(next_matrix.astype('float32'))
 
         self._current_angle = kornia.rotation_matrix_to_angle_axis(
             torch.from_numpy(current_matrix[:3, :3].astype('float32')).reshape(1, 3, 3)).permute(1, 0).squeeze()
@@ -61,8 +66,18 @@ class PoseDataPoint:
         """
         return {**self.get_delta_position(), **self.get_delta_angle()}
 
+    def get_current_transformation(self):
+        return {self._current_transformation_name: self._current_transformation}
+
+    def get_next_transformation(self):
+        return {self._next_transformation_name: self._next_transformation}
+
+    def get_transformation(self):
+        return {**self.get_current_transformation(), **self.get_next_transformation()}
+
     def get_data(self):
         """
         :return: dictionaries in format position, angle: 3 for each current, next and delta
         """
-        return {**self.get_current_state(), **self.get_next_state(), **self.get_delta_state()}
+        return {**self.get_current_state(), **self.get_next_state(), **self.get_delta_state(),
+                **self.get_transformation()}

undeepvo/models/pose_model.py
@@ -28,7 +28,7 @@ class PoseNet(nn.Module):
 
         self.rot1 = nn.Linear(n_base_channels*32*7*7, 512)
         self.rot2 = nn.Linear(512, 512)
-        self.rot3 = nn.Linear(512, 3)
+        self.rot3 = nn.Linear(512, 4)
 
         self.transl1 = nn.Linear(n_base_channels*32*7*7, 512)
         self.transl2 = nn.Linear(512, 512)
@@ -77,7 +77,7 @@ class PoseNetResNet(nn.Module):
 
         self.rot1 = nn.Linear(512 * 6 * 6, 512)
         self.rot2 = nn.Linear(512, 512)
-        self.rot3 = nn.Linear(512, 3)
+        self.rot3 = nn.Linear(512, 4)
 
         self.transl1 = nn.Linear(512 * 6 * 6, 512)
         self.transl2 = nn.Linear(512, 512)

undeepvo/problems/unsupervised_dataset_manager.py
@@ -1,16 +1,18 @@
+import numpy as np
+import torch
+from torch.utils.data import DataLoader, random_split
+
+from undeepvo.data.cameras_calibration import CamerasCalibration
 from undeepvo.data.datatransform_manager import DataTransformManager
 from undeepvo.data.stereo_dataset import StereoDataset
 from undeepvo.utils import DatasetManager
-from torch.utils.data import DataLoader, random_split
-import numpy as np
-import torch
-from undeepvo.data.cameras_calibration import CamerasCalibration
 
 
 class UnsupervisedDatasetManager(DatasetManager):
     def __init__(self, kitti_dataset, num_workers=4, lenghts=(80, 10, 10), final_img_size=(128, 384),
                  transform_params={"filters": True, "normalize": False}):
         dataset = StereoDataset(dataset=kitti_dataset)
+        self._kitti_dataset = kitti_dataset
         train, val, test = random_split(dataset, lenghts)
         self._num_workers = num_workers
         super().__init__(train, val, test)
@@ -54,7 +56,21 @@ class UnsupervisedDatasetManager(DatasetManager):
                                        [0., focal, cy],
                                        [0., 0., 1.]])
         right_camera_matrix = np.array([[focal, 0., cx],
-                                       [0., focal, cy],
-                                       [0., 0., 1.]])
+                                        [0., focal, cy],
+                                        [0., 0., 1.]])
         camera_baseline = 0.54
         return CamerasCalibration(camera_baseline, left_camera_matrix, right_camera_matrix, device)
+
+    def get_camera0_from_left_transformation(self, device="cuda:0"):
+        camera0_from_camera2_transformation = self._kitti_dataset.calib.T_cam0_velo.dot(
+            np.linalg.inv(self._kitti_dataset.calib.T_cam2_velo))
+        camera0_from_camera2_transformation = torch.from_numpy(camera0_from_camera2_transformation).to(device)[
+            None].float()
+        return camera0_from_camera2_transformation
+
+    def get_camera0_from_right_transformation(self, device="cuda:0"):
+        camera0_from_camera3_transformation = self._kitti_dataset.calib.T_cam0_velo.dot(
+            np.linalg.inv(self._kitti_dataset.calib.T_cam3_velo))
+        camera0_from_camera3_transformation = torch.from_numpy(camera0_from_camera3_transformation).to(device)[
+            None].float()
+        return camera0_from_camera3_transformation

undeepvo/utils/math.py
@@ -2,9 +2,8 @@ import kornia
 import torch
 
 
-def generate_transformation(translation, log_quaternion):
-    quaternion = kornia.quaternion_log_to_exp(log_quaternion)
-    rotation_matrix = kornia.geometry.quaternion_to_rotation_matrix(quaternion)  # current transformation matrix
+def generate_transformation(translation, rotation):
+    rotation_matrix = kornia.geometry.quaternion_to_rotation_matrix(rotation)  # current transformation matrix
     translation = translation.unsqueeze(dim=2)
     transform_matrix = torch.cat((rotation_matrix, translation), dim=2)
     tmp = torch.tensor([[0, 0, 0, 1]] * translation.shape[0], dtype=torch.float).to(translation.device)
@@ -16,9 +15,9 @@ def generate_transformation(translation, log_quaternion):
 def generate_pose(transformation):
     translation = transformation[:, :3, 3]
     rotation_matrix = transformation[:, :3, :3].clone()
-    quaternion = kornia.geometry.rotation_matrix_to_quaternion(rotation_matrix)
-    log_quaternion = kornia.quaternion_exp_to_log(quaternion)
-    return translation, log_quaternion
+    rotation = kornia.geometry.rotation_matrix_to_quaternion(rotation_matrix)
+    rotation = torch.where(rotation[:, 3] > 0, rotation, -rotation)
+    return translation, rotation
 
 
 def generate_relative_transformation(src_translation, src_rotation, dst_translation, dst_rotation):