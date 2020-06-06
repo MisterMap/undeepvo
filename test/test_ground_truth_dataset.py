f6328927f48f53ff91e74f1a1a449c58155423a3
Valery Ilin <valery.ilin@skoltech.ru>
2020-06-06 17:28:46 +0300

    supervised ground truth dataset: +velodyne image as depth (variative)

--- test/test_ground_truth_dataset.py
+++ test/test_ground_truth_dataset.py
@@ -8,7 +8,7 @@ from undeepvo.data.supervised import GroundTruthDataset
 class TestGroundTruthDataset(unittest.TestCase):
     def test_dataset(self):
         length = 1000
-        dataset = GroundTruthDataset(length=length)
+        dataset = GroundTruthDataset(velodyne=True, length=length)
         self.assertEqual(dataset.get_length(), length)
         idx = 0
         self.assertIsInstance(dataset.get_image(idx), np.ndarray)
